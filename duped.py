59f6c4313a6160f76b5420700082eb9159005d89
Josh Pullen <bolapara@gmail.com>
2019-05-24 01:05:51 -0500

    cleanup code

--- duped.py
+++ duped.py
@@ -4,6 +4,7 @@ import hashlib
 import os
 import sys
 import errno
+import time
 from multiprocessing import cpu_count
 from concurrent.futures import ProcessPoolExecutor
 
@@ -64,61 +65,85 @@ def decider(hash_dict, auto_delete_list):
     return keep_list, del_list
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--no-empty', action='store_true', help="Skip empty files")
-parser.add_argument('--skip', action='append', default=[],
-                    help="List of directory names to ignore")
-parser.add_argument(
-    '--auto-delete', action='append', default=[],
-    help="List of directories to automatically delete duplicates from"
-)
-parser.add_argument('--procs', type=int, default=cpu_count(),
-                    help="Number of processes to use")
-parser.add_argument('--verbose', action='store_true', help="Verbose mode")
-parser.add_argument('directories', nargs='+')
-args = parser.parse_args()
-if args.verbose:
-    print(args)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--no-empty', action='store_true', help="Skip empty files")
+    parser.add_argument('--skip', action='append', default=[],
+                        help="List of directory names to ignore")
+    parser.add_argument(
+        '--auto-delete', action='append', default=[],
+        help="List of directories to automatically delete duplicates from"
+    )
+    parser.add_argument('--procs', type=int, default=cpu_count(),
+                        help="Number of processes to use")
+    parser.add_argument('--verbose', action='store_true', help="Verbose mode")
+    parser.add_argument('directories', nargs='+')
+    args = parser.parse_args()
+    if args.verbose:
+        print(args)
+    return args
+
+
+def process_files(directories, args):
+    hash_dict, error_list = {}, []
+    with ProcessPoolExecutor(max_workers=args.procs) as executor:
+        count = 0
+        file_list = generate_file_list(directories, args.no_empty, args.skip)
+        for file_hash, filename in executor.map(hasher, file_list):
+            if not file_hash:
+                error_list.append(filename)
+            hashes = hash_dict.setdefault(file_hash, [])
+            hashes.append(filename)
+            count += 1
+            print('\r{}'.format(count), end='', flush=True)
+        print()
+    return hash_dict, error_list
+
+
+def write_results(keep_list, delete_list, error_list, hash_dict, timings, args):
+    extension = str(os.getpid())
+    with open('keep.{}'.format(extension), 'x') as fobj:
+        fobj.writelines(("{}\n".format(line) for line in keep_list))
+
+    with open('delete.{}'.format(extension), 'x') as fobj:
+        fobj.writelines(("{}\n".format(line) for line in delete_list))
+
+    with open('error.{}'.format(extension), 'x') as fobj:
+        fobj.writelines(("{}\n".format(line) for line in error_list))
+
+    with open('hashes.{}'.format(extension), 'x') as fobj:
+        for file_hash, filenames in hash_dict.items():
+            fobj.writelines('{} {}\n'.format(file_hash, filename)
+                            for filename in filenames)
+
+    with open('commandline.{}'.format(extension), 'x') as fobj:
+        fobj.write('{}\n'.format(args))
+
+    with open('runtime.{}'.format(extension), 'x') as fobj:
+        fobj.write('{}\n'.format(timings[1] - timings[0]))
+
+
+args = parse_args()
 
 if not os.access('.', os.W_OK):
     print("Error, no write access to current directory", file=sys.stderr)
     sys.exit(errno.EACCES)
 
-directories = [os.path.normpath(directory) for directory in args.directories]
-delete_dirs = [os.path.normpath(directory) for directory in args.auto_delete]
+start_time = time.perf_counter()
 
 print("processing files")
-hash_dict, error_list = {}, []
-with ProcessPoolExecutor(max_workers=args.procs) as executor:
-    count = 0
-    file_list = generate_file_list(directories, args.no_empty, args.skip)
-    for file_hash, filename in executor.map(hasher, file_list):
-        if not file_hash:
-            error_list.append(filename)
-        hashes = hash_dict.setdefault(file_hash, [])
-        hashes.append(filename)
-        count += 1
-        print('\r{}'.format(count), end='', flush=True)
-    print()
+hash_dict, error_list = process_files(
+        [os.path.normpath(directory) for directory in args.directories],
+        args
+    )
 
 print("analyzing files")
-keep_list, delete_list = decider(hash_dict, delete_dirs)
+keep_list, delete_list = decider(
+        hash_dict,
+        [os.path.normpath(directory) for directory in args.auto_delete]
+    )
+
+timings = (start_time, time.perf_counter())
 
 print("writing out results")
-extension = str(os.getpid())
-with open('keep.{}'.format(extension), 'x') as fobj:
-    fobj.writelines(("{}\n".format(line) for line in keep_list))
-
-with open('delete.{}'.format(extension), 'x') as fobj:
-    fobj.writelines(("{}\n".format(line) for line in delete_list))
-
-with open('error.{}'.format(extension), 'x') as fobj:
-    fobj.writelines(("{}\n".format(line) for line in error_list))
-
-with open('hashes.{}'.format(extension), 'x') as fobj:
-    for file_hash, filenames in hash_dict.items():
-        fobj.writelines('{} {}\n'.format(file_hash, filename)
-                        for filename in filenames)
-
-with open('commandline.{}'.format(extension), 'x') as fobj:
-    fobj.write('{}\n'.format(args))
+write_results(keep_list, delete_list, error_list, hash_dict, timings, args)
